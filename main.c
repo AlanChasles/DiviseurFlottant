a26505bb99486674c8fe3594b9345460a9d807f6
AlanChasles <alan.chasles@gmail.com>
2015-01-26 14:53:51 +0100

    azraz

--- main.c
+++ main.c
@@ -6,8 +6,10 @@ float f1,f2;
 
 cout << "Veuillez saisir deux nombres : ";
 cin >> f1;
+while(f2 == 0){
+cout << "Votre deuxième nombre est égal à 0, veuillez le resaisir : " << endl;
 cin >> f2;
-
+}
 cout << "Le quotient de la division du nombre " << f1 << " par " << f2 << " est " << f1/f2 << endl;
 
 return 0;
