2e345064b498b9487a0ebd62563488d1a04435c9
AlanChasles <alan.chasles@gmail.com>
2015-01-26 14:06:22 +0100

    azrar

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+float f1,f2;
+
+cout << "Veuillez saisir deux nombres : ";
+cin >> f1;
+cin >> f2;
+
+cout << "Le quotient de la division du nombre " << f1 << " par " << f2 << " est " << f1/f2 << endl;
+
+return 0;
+}
